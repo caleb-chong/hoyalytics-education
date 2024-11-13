53a0f4237bea8caa67992ba6b612781200d020ff
caleb-chong <cgc115@georgetown.edu>
2024-11-12 19:47:58 -0500

    Coding tmdb-demo.py

--- tmdb-demo.py
+++ tmdb-demo.py
@@ -1,1 +1,29 @@
-print("Hello World!")
+import os
+from dotenv import load_dotenv
+import requests
+import pandas as pd
+
+load_dotenv()
+
+API_key = os.getenv("API_key", default="demo")
+
+url = "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {API_key}"
+}
+
+response = requests.get(url, headers=headers)
+
+# Convert response to JSON
+data = response.json()
+print(data)
+
+# Use pandas
+# df = pd.DataFrame(data)
+# df.to_csv("trending_movies.csv", index=False)
+
+# Get results key
+# data = data["results"]
+# print(len(data))
